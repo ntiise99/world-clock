158f91a9a1461bc089021220125da7b4d35dda1c
Leseli <leselintiise@gmail.com>
2024-10-28 12:54:06 +0200

    Edited the background color for the body and the js

---
+++ Javascript/index.js
@@ -0,0 +1,108 @@
+function updateTime() {
+  let selectedCitiesContainerElement = document.querySelector(
+    "#selected-cities-container"
+  );
+  selectedCitiesContainerElement.innerHTML = `<div class="city-container">
+            <div>
+              <span class="city-name">New York City</span>
+              <br />
+              <span class="date">
+                ${moment().tz("America/New_York").format("MMMM D, YYYY")}
+              </span>
+            </div>
+            <div class="time">${moment()
+              .tz("America/New_York")
+              .format("h:mm:ss")} <small>${moment()
+    .tz("America/New_York")
+    .format("A")}</small>
+            </div>
+          </div>
+          <div class="city-container">
+            <div>
+              <span class="city-name">Shanghai</span>
+              <br />
+              <span class="date">
+                ${moment().tz("Asia/Shanghai").format("MMMM D, YYYY")}
+              </span>
+            </div>
+            <div class="time">${moment()
+              .tz("Asia/Shanghai")
+              .format("h:mm:ss")} <small>${moment()
+    .tz("Asia/Shanghai")
+    .format("A")}</small>
+            </div>
+          </div>
+          <div class="city-container">
+            <div>
+              <span class="city-name">Paris</span>
+              <br />
+              <span class="date">
+                ${moment().tz("Europe/Paris").format("MMMM D, YYYY")}
+              </span>
+            </div>
+            <div class="time">${moment()
+              .tz("Europe/Paris")
+              .format("h:mm:ss")} <small>${moment()
+    .tz("Europe/Paris")
+    .format("A")}</small>
+            </div>
+          </div>
+          <div class="city-container">
+            <div>
+              <span class="city-name">Tokyo</span>
+              <br />
+              <span class="date">
+                ${moment().tz("Asia/Tokyo").format("MMMM D, YYYY")}
+              </span>
+            </div>
+            <div class="time">${moment()
+              .tz("Asia/Tokyo")
+              .format("h:mm:ss")} <small>${moment()
+    .tz("Asia/Tokyo")
+    .format("A")}</small>
+            </div>
+          </div>
+          `;
+}
+
+function changeCityRepeat(event) {
+  selectCityInterval = setInterval(changeCity, 1000);
+  function changeCity() {
+    clearInterval(allCitiesInterval);
+    let timezone = event.target.value;
+    if (timezone === "current") {
+      timezone = moment.tz.guess();
+    }
+    let cityName = timezone
+      .replace("_", " ")
+      .replace("Kolkata", "Mumbai")
+      .split("/")[1];
+
+    let selectedCitiesContainerElement = document.querySelector(
+      "#selected-cities-container"
+    );
+    selectedCitiesContainerElement.innerHTML = `
+  <div class="city-container">
+            <div>
+              <span class="city-name">${cityName}</span>
+              <br />
+              <span class="date">
+                ${moment().tz(timezone).format("MMMM D, YYYY")}
+              </span>
+            </div>
+            <div class="time">${moment()
+              .tz(timezone)
+              .format("h:mm:ss")} <small>${moment()
+      .tz(timezone)
+      .format("A")}</small>
+            </div>
+          </div>
+          <a href="/"><button class="all-cites" href="/">Show All Cities</button></a>`;
+  }
+}
+
+updateTime();
+allCitiesInterval = setInterval(updateTime, 1000);
+
+let citiesSelectorElement = document.querySelector("#cities-selector");
+citiesSelectorElement.addEventListener("change", changeCityRepeat);
